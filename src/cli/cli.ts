c9252687db70ae02aa87b6dbaed2c26456b4a976
GHOSt <ghostdevbusiness@gmail.com>
2022-05-27 20:34:02 +0100

    feat: run command

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -1,31 +1,43 @@
-import { run } from '../compilers/console';
-import { Program } from '../parser';
+import * as consoleCompiler from '../compilers/console';
+import { Program } from '../parser/index';
 import { readFileSync } from 'fs';
 import minimist from 'minimist';
 import { resolve } from 'path';
 import kleur from 'kleur';
 
 const args = minimist(process.argv.slice(2));
-const [file] = args._;
+const [command] = args._;
 
-if (!file) throw new Error('Please give a file');
-
-const path = resolve(file);
-const data = readFileSync(path, 'utf8');
-
-const program = new Program();
-
-program.add(data);
-
-if (args.debug) {
-    const line = '======================================================';
-
-    console.log();
-    console.log(kleur.bold().green('Debug'));
-    console.log(kleur.dim(line));
-    console.log(program);
-    console.log(kleur.dim(line));
-    console.log();
+if (!command || !command.length) {
+    throw new Error('No command specified');
 }
 
-run(program);
+switch (command) {
+    case 'run': {
+        const [, file] = args._;
+
+        if (!file) throw new Error('Please give a file');
+
+        const path = resolve(file);
+        const data = readFileSync(path, 'utf8');
+
+        const program = new Program();
+
+        program.add(data);
+
+        if (args.debug) {
+            const line = '='.repeat(15);
+
+            console.log();
+            console.log(kleur.bold().green('Debug'));
+            console.log(kleur.dim(line));
+            console.log(program);
+            console.log(kleur.dim(line));
+            console.log();
+        }
+
+        consoleCompiler.run(program);
+
+        break;
+    }
+}
